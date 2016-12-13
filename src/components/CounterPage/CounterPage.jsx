ba70235d693adc5375af95733ec97785ae8a2122
s-mironov31 <s.mironov3112@gmail.com>
2016-12-13 17:28:58 +0400

    'StateCounte'

--- src/components/CounterPage/CounterPage.jsx
+++ src/components/CounterPage/CounterPage.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import PageHeader from 'react-bootstrap/lib/PageHeader';
+import StateCounter from './StateCounter';
 
 class CounterPage extends Component {
   render() {
     return (
-      <div>Заглушка для счетчиков</div>
+      <div>
+        <PageHeader>Counters</PageHeader>
+        <h3>State Counter</h3>
+        <StateCounter />
+      </div>
     );
   }
 }
